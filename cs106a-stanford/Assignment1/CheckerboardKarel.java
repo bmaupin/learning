1ce48bd4a22be35e0dfac931e04c7d899cc8aaa9
bmaupin <bmaupin@users.noreply.github.com>
2016-09-01 13:40:36 -0400

    Add first assignment for cs106a

---
+++ cs106a-stanford/Assignment1/CheckerboardKarel.java
@@ -0,0 +1,16 @@
+/*
+ * File: CheckerboardKarel.java
+ * ----------------------------
+ * When you finish writing it, the CheckerboardKarel class should draw
+ * a checkerboard using beepers, as described in Assignment 1.  You
+ * should make sure that your program works for all of the sample
+ * worlds supplied in the starter folder.
+ */
+
+import stanford.karel.*;
+
+public class CheckerboardKarel extends SuperKarel {
+
+	// You fill in this part
+
+}
---
+++ cs106a-stanford/Assignment1/StoneMasonKarel.java
@@ -0,0 +1,17 @@
+/*
+ * File: StoneMasonKarel.java
+ * --------------------------
+ * The StoneMasonKarel subclass as it appears here does nothing.
+ * When you finish writing it, it should solve the "repair the quad"
+ * problem from Assignment 1.  In addition to editing the program,
+ * you should be sure to edit this comment so that it no longer
+ * indicates that the program does nothing.
+ */
+
+import stanford.karel.*;
+
+public class StoneMasonKarel extends SuperKarel {
+
+	// You fill in this part
+
+}
