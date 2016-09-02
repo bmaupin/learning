07a9aba71bf7798169b048c35efc8de091507234
bmaupin <bmaupin@users.noreply.github.com>
2016-09-02 14:58:47 -0400

    Add second, less elegant solution for assignment 1, problem 4

---
+++ cs106a-stanford/Assignment1/MidpointFindingKarel2.java
@@ -0,0 +1,61 @@
+/*
+ * File: MidpointFindingKarel.java
+ * -------------------------------
+ * When you finish writing it, the MidpointFindingKarel class should
+ * leave a beeper on the corner closest to the center of 1st Street
+ * (or either of the two central corners if 1st Street has an even
+ * number of corners).  Karel can put down additional beepers as it
+ * looks for the midpoint, but must pick them up again before it
+ * stops.  The world may be of any size, but you are allowed to
+ * assume that it is at least as tall as it is wide.
+ */
+
+import stanford.karel.*;
+
+/* Note: this fails if the world is only one street wide. I'm not sure if that's 
+ * permissible or not.
+ */
+public class MidpointFindingKarel2 extends SuperKarel {
+	public void run() {
+		setup();
+		while (frontIsBlocked()) {
+			turnAround();
+			moveToFirstBeeper();
+			grabAndGo();
+		}
+	}
+	
+	private void setup() {
+		while (frontIsClear()) {
+			putBeeper();
+			move();
+		}
+		putBeeper();
+	}
+	
+	private void moveToWall() {
+		while (frontIsClear()) {
+			move();
+		}
+	}
+	
+	private void grabAndGo() {
+		if (beepersPresent()) {
+			pickBeeper();
+			move();
+			if (beepersPresent()) {
+				moveToWall();
+			} else {
+				turnAround();
+				move();
+				putBeeper();
+			}
+		}
+	}
+	
+	private void moveToFirstBeeper() {
+		while (noBeepersPresent()) {
+			move();
+		}
+	}
+}
