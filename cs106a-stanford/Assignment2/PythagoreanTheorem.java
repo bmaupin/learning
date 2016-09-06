fc9cfe18700de46d88d9854e8ee14e3e1d092f8d
bmaupin <bmaupin@users.noreply.github.com>
2016-09-06 14:41:43 -0400

    Finish assignment 2, problem 4

--- cs106a-stanford/Assignment2/PythagoreanTheorem.java
+++ cs106a-stanford/Assignment2/PythagoreanTheorem.java
@@ -1,15 +1,29 @@
+import java.util.Scanner;
+
 /*
  * File: PythagoreanTheorem.java
- * Name: 
- * Section Leader: 
+ * Name:
+ * Section Leader:
  * -----------------------------
  * This file is the starter file for the PythagoreanTheorem problem.
  */
 
-import acm.program.*;
+public class PythagoreanTheorem {
+    private static Scanner scanner;
 
-public class PythagoreanTheorem extends ConsoleProgram {
-    public void run() {
-        /* You fill this in */
+    public static void main(String[] args) {
+        System.out.println("Enter values to compute Pythagorean theoroem.");
+
+        scanner = new Scanner(System.in);
+
+        System.out.print("a: ");
+        int a = scanner.nextInt();
+
+        System.out.print("b: ");
+        int b = scanner.nextInt();
+
+        double c = Math.sqrt(a * a + b * b);
+
+        System.out.println(String.format("c = %f", c));
     }
 }
