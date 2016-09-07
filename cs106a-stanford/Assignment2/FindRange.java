5bb640654b174fa9cca62b070ffd506549365432
bmaupin <bmaupin@users.noreply.github.com>
2016-09-07 14:14:16 -0400

    Finish assignment 2, problem 5

--- cs106a-stanford/Assignment2/FindRange.java
+++ cs106a-stanford/Assignment2/FindRange.java
@@ -1,16 +1,72 @@
+
 /*
  * File: FindRange.java
- * Name: 
- * Section Leader: 
+ * Name:
+ * Section Leader:
  * --------------------
  * This file is the starter file for the FindRange problem.
  */
 
-import acm.program.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class FindRange extends ConsoleProgram {
-    public void run() {
-        /* You fill this in */
+public class FindRange {
+    public static final int SENTINEL = 0;
+
+    private static Scanner scanner;
+
+    public static void main(String[] args) {
+        System.out.println("This program finds the largest and smallest numbers.");
+
+        List<Integer> values = getValuesFromUser();
+        printExtrema(values);
+    }
+
+    private static List<Integer> getValuesFromUser() {
+        List<Integer> values = new ArrayList<Integer>();
+        scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("? ");
+            int value = scanner.nextInt();
+
+            if (value == SENTINEL) {
+                break;
+            }
+
+            values.add(value);
+        }
+
+        return values;
+    }
+
+    private static void printExtrema(List<Integer> values) {
+        if (values.size() == 0) {
+            System.out.println("No values have been entered.");
+
+        } else {
+            int largestValue = 0;
+            int smallestValue = 0;
+
+            for (int i = 0; i < values.size(); i++) {
+                if (i == 0) {
+                    largestValue = values.get(i);
+                    smallestValue = largestValue;
+
+                } else {
+                    if (values.get(i) < smallestValue) {
+                        smallestValue = values.get(i);
+                    }
+
+                    if (values.get(i) > largestValue) {
+                        largestValue = values.get(i);
+                    }
+                }
+            }
+
+            System.out.println(String.format("smallest: %d", smallestValue));
+            System.out.println(String.format("largest: %d", largestValue));
+        }
     }
 }
-
