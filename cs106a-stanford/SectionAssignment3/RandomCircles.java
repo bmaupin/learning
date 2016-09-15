36e83f9b9c6ee9b02ad7170485b0219eab0a6e39
bmaupin <bmaupin@users.noreply.github.com>
2016-09-15 14:30:45 -0400

    Create starter files for section assignment 3

---
+++ cs106a-stanford/SectionAssignment3/RandomCircles.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class RandomCircles extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // TODO Auto-generated method stub
+    }
+}
