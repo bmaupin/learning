01b48751e9fc6216d97a9ed4c8740ec0e34e69ab
bmaupin <bmaupin@users.noreply.github.com>
2016-09-19 14:34:24 -0400

    Finish section assignment 4, problem 1

--- cs106a-stanford/SectionAssignment4/AddCommas.java
+++ cs106a-stanford/SectionAssignment4/AddCommas.java
@@ -1,5 +1,37 @@
-public class AddCommas {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class AddCommas {
+    private static final String NUMBER_GROUP_SEPARATOR = ",";
+    private static final int NUMBER_GROUP_SIZE = 3;
+
+    private static Scanner scanner;
+
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Enter a number: ");
+            String digits = scanner.nextLine();
+
+            if (digits.length() == 0) {
+                break;
+            }
+
+            System.out.println(addCommasToNumericString(digits));
+        }
+    }
+
+    private static String addCommasToNumericString(String digits) {
+        String formattedString = "";
+
+        for (int i = 0; i < digits.length(); i++) {
+            if (i > 0 && i % NUMBER_GROUP_SIZE == 0) {
+                formattedString = NUMBER_GROUP_SEPARATOR + formattedString;
+            }
+
+            formattedString = digits.charAt(digits.length() - 1 - i) + formattedString;
+        }
+
+        return formattedString;
     }
 }
