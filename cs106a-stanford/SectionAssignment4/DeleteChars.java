60953e410cf29ce53176c9c5f26491e9fec18b65
bmaupin <bmaupin@users.noreply.github.com>
2016-09-19 14:44:08 -0400

    Finish section assignment 4, problem 2

--- cs106a-stanford/SectionAssignment4/DeleteChars.java
+++ cs106a-stanford/SectionAssignment4/DeleteChars.java
@@ -1,5 +1,11 @@
 public class DeleteChars {
     public static void main(String[] args) {
+        System.out.println(removeAllOccurrences("This is a test", 't'));
+        System.out.println(removeAllOccurrences("Summer is here!", 'e'));
+        System.out.println(removeAllOccurrences("---0---", '-'));
+    }
 
+    public static String removeAllOccurrences(String str, char ch) {
+        return str.replaceAll(Character.toString(ch), "");
     }
 }
