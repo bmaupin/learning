2716022246a5f17954e72991b8ee18478192c417
bmaupin <bmaupin@users.noreply.github.com>
2016-09-20 13:20:27 -0400

    Remove ACM from HangmanLexicon

--- cs106a-stanford/Assignment4/HangmanLexicon.java
+++ cs106a-stanford/Assignment4/HangmanLexicon.java
@@ -5,29 +5,41 @@
  * class that you will reimplement for Part III of the assignment.
  */
 
-import acm.util.*;
-
 public class HangmanLexicon {
+    /** Returns the number of words in the lexicon. */
+    public int getWordCount() {
+        return 10;
+    }
 
-/** Returns the number of words in the lexicon. */
-	public int getWordCount() {
-		return 10;
-	}
-
-/** Returns the word at the specified index. */
-	public String getWord(int index) {
-		switch (index) {
-			case 0: return "BUOY";
-			case 1: return "COMPUTER";
-			case 2: return "CONNOISSEUR";
-			case 3: return "DEHYDRATE";
-			case 4: return "FUZZY";
-			case 5: return "HUBBUB";
-			case 6: return "KEYHOLE";
-			case 7: return "QUAGMIRE";
-			case 8: return "SLITHER";
-			case 9: return "ZIRCON";
-			default: throw new ErrorException("getWord: Illegal index");
-		}
-	};
+    /**
+     * Returns the word at the specified index.
+     * 
+     * @throws Exception
+     */
+    public String getWord(int index) throws Exception {
+        switch (index) {
+        case 0:
+            return "BUOY";
+        case 1:
+            return "COMPUTER";
+        case 2:
+            return "CONNOISSEUR";
+        case 3:
+            return "DEHYDRATE";
+        case 4:
+            return "FUZZY";
+        case 5:
+            return "HUBBUB";
+        case 6:
+            return "KEYHOLE";
+        case 7:
+            return "QUAGMIRE";
+        case 8:
+            return "SLITHER";
+        case 9:
+            return "ZIRCON";
+        default:
+            throw new Exception("getWord: Illegal index");
+        }
+    };
 }
