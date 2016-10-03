d1d01327287f3f65d445186852580d50cb98f31a
bmaupin <bmaupin@users.noreply.github.com>
2016-10-03 13:43:55 -0400

    Draw body parts

--- cs106a-stanford/Assignment4/HangmanCanvas.java
+++ cs106a-stanford/Assignment4/HangmanCanvas.java
@@ -8,6 +8,8 @@
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
@@ -43,6 +45,64 @@ public class HangmanCanvas {
     private static final double ROPE_START_Y = HORIZONTAL_BEAM_END_Y;
     private static final double ROPE_END_Y = ROPE_START_Y + ROPE_LENGTH;
 
+    private static final double HEAD_CENTER_X = ROPE_END_X;
+    private static final double HEAD_CENTER_Y = ROPE_END_Y + HEAD_RADIUS;
+
+    private static final double BODY_START_X = HEAD_CENTER_X;
+    private static final double BODY_END_X = HEAD_CENTER_X;
+    private static final double BODY_START_Y = HEAD_CENTER_Y + HEAD_RADIUS;
+    private static final double BODY_END_Y = BODY_START_Y + BODY_LENGTH;
+
+    private static final double LEFT_UPPER_ARM_START_X = BODY_START_X;
+    private static final double LEFT_UPPER_ARM_END_X = LEFT_UPPER_ARM_START_X - UPPER_ARM_LENGTH;
+    private static final double LEFT_UPPER_ARM_START_Y = BODY_START_Y + ARM_OFFSET_FROM_HEAD;
+    private static final double LEFT_UPPER_ARM_END_Y = LEFT_UPPER_ARM_START_Y;
+
+    private static final double LEFT_LOWER_ARM_START_X = LEFT_UPPER_ARM_END_X;
+    private static final double LEFT_LOWER_ARM_END_X = LEFT_LOWER_ARM_START_X;
+    private static final double LEFT_LOWER_ARM_START_Y = LEFT_UPPER_ARM_END_Y;
+    private static final double LEFT_LOWER_ARM_END_Y = LEFT_LOWER_ARM_START_Y + LOWER_ARM_LENGTH;
+
+    private static final double RIGHT_UPPER_ARM_START_X = LEFT_UPPER_ARM_START_X;
+    private static final double RIGHT_UPPER_ARM_END_X = RIGHT_UPPER_ARM_START_X + UPPER_ARM_LENGTH;
+    private static final double RIGHT_UPPER_ARM_START_Y = LEFT_UPPER_ARM_START_Y;
+    private static final double RIGHT_UPPER_ARM_END_Y = RIGHT_UPPER_ARM_START_Y;
+
+    private static final double RIGHT_LOWER_ARM_START_X = RIGHT_UPPER_ARM_END_X;
+    private static final double RIGHT_LOWER_ARM_END_X = RIGHT_LOWER_ARM_START_X;
+    private static final double RIGHT_LOWER_ARM_START_Y = RIGHT_UPPER_ARM_END_Y;
+    private static final double RIGHT_LOWER_ARM_END_Y = RIGHT_LOWER_ARM_START_Y + LOWER_ARM_LENGTH;
+
+    private static final double LEFT_UPPER_LEG_START_X = BODY_END_X;
+    private static final double LEFT_UPPER_LEG_END_X = LEFT_UPPER_LEG_START_X - HIP_WIDTH;
+    private static final double LEFT_UPPER_LEG_START_Y = BODY_END_Y;
+    private static final double LEFT_UPPER_LEG_END_Y = LEFT_UPPER_LEG_START_Y;
+
+    private static final double LEFT_LOWER_LEG_START_X = LEFT_UPPER_LEG_END_X;
+    private static final double LEFT_LOWER_LEG_END_X = LEFT_LOWER_LEG_START_X;
+    private static final double LEFT_LOWER_LEG_START_Y = LEFT_UPPER_LEG_END_Y;
+    private static final double LEFT_LOWER_LEG_END_Y = LEFT_LOWER_LEG_START_Y + LEG_LENGTH;
+
+    private static final double RIGHT_UPPER_LEG_START_X = LEFT_UPPER_LEG_START_X;
+    private static final double RIGHT_UPPER_LEG_END_X = RIGHT_UPPER_LEG_START_X + HIP_WIDTH;
+    private static final double RIGHT_UPPER_LEG_START_Y = LEFT_UPPER_LEG_START_Y;
+    private static final double RIGHT_UPPER_LEG_END_Y = RIGHT_UPPER_LEG_START_Y;
+
+    private static final double RIGHT_LOWER_LEG_START_X = RIGHT_UPPER_LEG_END_X;
+    private static final double RIGHT_LOWER_LEG_END_X = RIGHT_LOWER_LEG_START_X;
+    private static final double RIGHT_LOWER_LEG_START_Y = RIGHT_UPPER_LEG_END_Y;
+    private static final double RIGHT_LOWER_LEG_END_Y = RIGHT_LOWER_LEG_START_Y + LEG_LENGTH;
+
+    private static final double LEFT_FOOT_START_X = LEFT_LOWER_LEG_END_X;
+    private static final double LEFT_FOOT_END_X = LEFT_FOOT_START_X - FOOT_LENGTH;
+    private static final double LEFT_FOOT_START_Y = LEFT_LOWER_LEG_END_Y;
+    private static final double LEFT_FOOT_END_Y = LEFT_FOOT_START_Y;
+
+    private static final double RIGHT_FOOT_START_X = RIGHT_LOWER_LEG_END_X;
+    private static final double RIGHT_FOOT_END_X = RIGHT_FOOT_START_X + FOOT_LENGTH;
+    private static final double RIGHT_FOOT_START_Y = RIGHT_LOWER_LEG_END_Y;
+    private static final double RIGHT_FOOT_END_Y = RIGHT_FOOT_START_Y;
+
     private Group rootGroup;
 
     public HangmanCanvas(Stage primaryStage) {
@@ -66,7 +126,7 @@ public class HangmanCanvas {
      * unguessed letters are indicated by hyphens.
      */
     public void displayWord(String word) {
-        /* You fill this in */
+        // TODO: implement this
     }
 
     /**
@@ -76,7 +136,15 @@ public class HangmanCanvas {
      * bottom of the window.
      */
     public void noteIncorrectGuess(char letter) {
-        /* You fill this in */
+        // TODO: add each body part one at a time
+        drawHead();
+        drawBody();
+        drawLeftArm();
+        drawRightArm();
+        drawLeftLeg();
+        drawRightLeg();
+        drawLeftFoot();
+        drawRightFoot();
     }
 
     private void drawScaffold() {
@@ -100,5 +168,66 @@ public class HangmanCanvas {
     private void drawRope() {
         Line rope = new Line(ROPE_START_X, ROPE_START_Y, ROPE_END_X, ROPE_END_Y);
         rootGroup.getChildren().add(rope);
+    }
+
+    private void drawHead() {
+        Circle head = new Circle(HEAD_CENTER_X, HEAD_CENTER_Y, HEAD_RADIUS, Color.TRANSPARENT);
+        head.setStroke(Color.BLACK);
+        rootGroup.getChildren().add(head);
+    }
+
+    private void drawBody() {
+        Line body = new Line(BODY_START_X, BODY_START_Y, BODY_END_X, BODY_END_Y);
+        rootGroup.getChildren().add(body);
+    }
+
+    private void drawLeftArm() {
+        Line leftUpperArm = new Line(LEFT_UPPER_ARM_START_X, LEFT_UPPER_ARM_START_Y, LEFT_UPPER_ARM_END_X,
+                LEFT_UPPER_ARM_END_Y);
+        Line leftLowerArm = new Line(LEFT_LOWER_ARM_START_X, LEFT_LOWER_ARM_START_Y, LEFT_LOWER_ARM_END_X,
+                LEFT_LOWER_ARM_END_Y);
+
+        Group leftArm = new Group(leftUpperArm, leftLowerArm);
+        rootGroup.getChildren().add(leftArm);
+    }
+
+    private void drawRightArm() {
+        Line rightUpperArm = new Line(RIGHT_UPPER_ARM_START_X, RIGHT_UPPER_ARM_START_Y, RIGHT_UPPER_ARM_END_X,
+                RIGHT_UPPER_ARM_END_Y);
+        Line rightLowerArm = new Line(RIGHT_LOWER_ARM_START_X, RIGHT_LOWER_ARM_START_Y, RIGHT_LOWER_ARM_END_X,
+                RIGHT_LOWER_ARM_END_Y);
+
+        Group rightArm = new Group(rightUpperArm, rightLowerArm);
+        rootGroup.getChildren().add(rightArm);
+    }
+
+    private void drawLeftLeg() {
+        Line leftUpperLeg = new Line(LEFT_UPPER_LEG_START_X, LEFT_UPPER_LEG_START_Y, LEFT_UPPER_LEG_END_X,
+                LEFT_UPPER_LEG_END_Y);
+        Line leftLowerLeg = new Line(LEFT_LOWER_LEG_START_X, LEFT_LOWER_LEG_START_Y, LEFT_LOWER_LEG_END_X,
+                LEFT_LOWER_LEG_END_Y);
+
+        Group leftLeg = new Group(leftUpperLeg, leftLowerLeg);
+        rootGroup.getChildren().add(leftLeg);
+    }
+
+    private void drawRightLeg() {
+        Line rightUpperLeg = new Line(RIGHT_UPPER_LEG_START_X, RIGHT_UPPER_LEG_START_Y, RIGHT_UPPER_LEG_END_X,
+                RIGHT_UPPER_LEG_END_Y);
+        Line rightLowerLeg = new Line(RIGHT_LOWER_LEG_START_X, RIGHT_LOWER_LEG_START_Y, RIGHT_LOWER_LEG_END_X,
+                RIGHT_LOWER_LEG_END_Y);
+
+        Group rightLeg = new Group(rightUpperLeg, rightLowerLeg);
+        rootGroup.getChildren().add(rightLeg);
+    }
+
+    private void drawLeftFoot() {
+        Line leftFoot = new Line(LEFT_FOOT_START_X, LEFT_FOOT_START_Y, LEFT_FOOT_END_X, LEFT_FOOT_END_Y);
+        rootGroup.getChildren().add(leftFoot);
+    }
+
+    private void drawRightFoot() {
+        Line rightFoot = new Line(RIGHT_FOOT_START_X, RIGHT_FOOT_START_Y, RIGHT_FOOT_END_X, RIGHT_FOOT_END_Y);
+        rootGroup.getChildren().add(rightFoot);
     }
 }
