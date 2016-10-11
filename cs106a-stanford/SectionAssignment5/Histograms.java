1abc7a9eed22061dae9280bc6d815ec0bf56fc86
bmaupin <bmaupin@users.noreply.github.com>
2016-10-11 13:55:57 -0400

    Finish section assignment 5 part 2

--- cs106a-stanford/SectionAssignment5/Histograms.java
+++ cs106a-stanford/SectionAssignment5/Histograms.java
@@ -1,5 +1,53 @@
-public class Histograms {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 
+public class Histograms {
+    private static final String INPUT_FILENAME = "MidtermScores.txt";
+    private static final int MAX_VALUE = 100;
+    private static final int VALUE_RANGE = 10;
+
+    private String[] histogramRanges;
+    private BufferedReader reader;
+
+    public static void main(String[] args) {
+        Histograms histograms = new Histograms();
+        histograms.run();
+    }
+
+    private void run() {
+        readInputFile();
+        displayHistogram();
+    }
+
+    private void readInputFile() {
+        histogramRanges = new String[MAX_VALUE / VALUE_RANGE + 1];
+        Arrays.fill(histogramRanges, "");
+
+        try {
+            reader = new BufferedReader(new FileReader(INPUT_FILENAME));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                int value = Integer.parseInt(line);
+                histogramRanges[value / VALUE_RANGE] += "*";
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void displayHistogram() {
+        int histogramRangeStart = 0;
+        int histogramRangeEnd = histogramRangeStart + VALUE_RANGE - 1;
+
+        for (String histogramRange : histogramRanges) {
+            String histogramRangeLabel = String.format("%02d-%02d", histogramRangeStart, histogramRangeEnd);
+
+            System.out.printf("%s: %s\n", histogramRangeLabel, histogramRange);
+
+            histogramRangeStart += VALUE_RANGE;
+            histogramRangeEnd += VALUE_RANGE;
+        }
     }
 }
