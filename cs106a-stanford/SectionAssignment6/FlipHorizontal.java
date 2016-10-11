63feb136039bf5c7e9898d19dbfe194e3bc406ad
bmaupin <bmaupin@users.noreply.github.com>
2016-10-11 14:19:43 -0400

    Add starter files for section assignment 6

---
+++ cs106a-stanford/SectionAssignment6/FlipHorizontal.java
@@ -0,0 +1,5 @@
+public class FlipHorizontal() {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ cs106a-stanford/SectionAssignment6/NameCounts.java
@@ -0,0 +1,5 @@
+public class NameCounts() {
+    public static void main(String[] args) {
+        
+    }
+}
