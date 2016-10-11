1e8fd60cd8a348e0aca2a436ff34a47f626c2ad4
bmaupin <bmaupin@users.noreply.github.com>
2016-10-11 14:12:37 -0400

    Finish section assignment 5 part 3

--- cs106a-stanford/SectionAssignment5/UniqueNames.java
+++ cs106a-stanford/SectionAssignment5/UniqueNames.java
@@ -1,5 +1,42 @@
-public class UniqueNames {
-    public static void main(String[] args) {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class UniqueNames {
+    private Scanner scanner;
+    private List<String> uniqueNames;
+
+    public static void main(String[] args) {
+        UniqueNames uniqueNames = new UniqueNames();
+        uniqueNames.run();
+    }
+
+    private void run() {
+        getNamesFromUser();
+        printUniqueNames();
+    }
+
+    private void getNamesFromUser() {
+        uniqueNames = new ArrayList<String>();
+        scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Enter name: ");
+            String nameFromUser = scanner.nextLine();
+            if (nameFromUser.equals("")) {
+                break;
+            }
+
+            if (!uniqueNames.contains(nameFromUser)) {
+                uniqueNames.add(nameFromUser);
+            }
+        }
+    }
+
+    private void printUniqueNames() {
+        System.out.println("Unique name list contains:");
+        for (String uniqueName : uniqueNames) {
+            System.out.println(uniqueName);
+        }
     }
 }
