c760cf313f52d14d620588d3b652108443643ce6
bmaupin <bmaupin@users.noreply.github.com>
2016-10-13 13:01:09 -0400

    Add starter code for assignment 5

---
+++ cs106a-stanford/Assignment5/Yahtzee.java
@@ -0,0 +1,38 @@
+/*
+ * File: Yahtzee.java
+ * ------------------
+ * This program will eventually play the Yahtzee game.
+ */
+
+import acm.io.*;
+import acm.program.*;
+import acm.util.*;
+
+public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
+	
+	public static void main(String[] args) {
+		new Yahtzee().start(args);
+	}
+	
+	public void run() {
+		IODialog dialog = getDialog();
+		nPlayers = dialog.readInt("Enter number of players");
+		playerNames = new String[nPlayers];
+		for (int i = 1; i <= nPlayers; i++) {
+			playerNames[i - 1] = dialog.readLine("Enter name for player " + i);
+		}
+		display = new YahtzeeDisplay(getGCanvas(), playerNames);
+		playGame();
+	}
+
+	private void playGame() {
+		/* You fill this in */
+	}
+		
+/* Private instance variables */
+	private int nPlayers;
+	private String[] playerNames;
+	private YahtzeeDisplay display;
+	private RandomGenerator rgen = new RandomGenerator();
+
+}
