c73789bc3f3c53a2c59d5954130814ccffa393cf
bmaupin <bmaupin@users.noreply.github.com>
2016-11-03 13:12:07 -0400

    Implement assignment 6 milestone 2

--- cs106a-stanford/Assignment6/NameSurferEntry.java
+++ cs106a-stanford/Assignment6/NameSurferEntry.java
@@ -1,3 +1,4 @@
+
 /*
  * File: NameSurferEntry.java
  * --------------------------
@@ -6,52 +7,55 @@
  * of that name for each decade stretching back to 1900.
  */
 
-import acm.util.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NameSurferEntry implements NameSurferConstants {
+    private List<Integer> decades;
+    private String name;
 
-/* Constructor: NameSurferEntry(line) */
-/**
- * Creates a new NameSurferEntry from a data line as it appears
- * in the data file.  Each line begins with the name, which is
- * followed by integers giving the rank of that name for each
- * decade.
- */
-	public NameSurferEntry(String line) {
-		// You fill this in //
-	}
+    /**
+     * Creates a new NameSurferEntry from a data line as it appears in the data
+     * file. Each line begins with the name, which is followed by integers
+     * giving the rank of that name for each decade.
+     */
+    public NameSurferEntry(String line) {
+        String[] splitLine = line.split("\\s+");
+        name = splitLine[0];
 
-/* Method: getName() */
-/**
- * Returns the name associated with this entry.
- */
-	public String getName() {
-		// You need to turn this stub into a real implementation //
-		return null;
-	}
+        decades = new ArrayList<Integer>();
+        for (int i = 1; i < splitLine.length; i++) {
+            decades.add(Integer.parseInt(splitLine[i]));
+        }
+    }
 
-/* Method: getRank(decade) */
-/**
- * Returns the rank associated with an entry for a particular
- * decade.  The decade value is an integer indicating how many
- * decades have passed since the first year in the database,
- * which is given by the constant START_DECADE.  If a name does
- * not appear in a decade, the rank value is 0.
- */
-	public int getRank(int decade) {
-		// You need to turn this stub into a real implementation //
-		return 0;
-	}
+    /**
+     * Returns the name associated with this entry.
+     */
+    public String getName() {
+        return name;
+    }
 
-/* Method: toString() */
-/**
- * Returns a string that makes it easy to see the value of a
- * NameSurferEntry.
- */
-	public String toString() {
-		// You need to turn this stub into a real implementation //
-		return "";
-	}
+    /**
+     * Returns the rank associated with an entry for a particular decade. The
+     * decade value is an integer indicating how many decades have passed since
+     * the first year in the database, which is given by the constant
+     * START_DECADE. If a name does not appear in a decade, the rank value is 0.
+     */
+    public int getRank(int decade) {
+        if (decade < decades.size()) {
+            return decades.get(decade);
+        }
+
+        return 0;
+    }
+
+    /**
+     * Returns a string that makes it easy to see the value of a
+     * NameSurferEntry.
+     */
+    @Override
+    public String toString() {
+        return String.format("%s %s", name, decades);
+    }
 }
-
