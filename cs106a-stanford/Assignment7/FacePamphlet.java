c841a73d39314ac1339740c0433e69245fa0595d
bmaupin <bmaupin@users.noreply.github.com>
2016-11-16 13:48:16 -0500

    Add starter code for assignment 7

---
+++ cs106a-stanford/Assignment7/FacePamphlet.java
@@ -0,0 +1,36 @@
+/* 
+ * File: FacePamphlet.java
+ * -----------------------
+ * When it is finished, this program will implement a basic social network
+ * management system.
+ */
+
+import acm.program.*;
+import acm.graphics.*;
+import acm.util.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class FacePamphlet extends Program 
+					implements FacePamphletConstants {
+
+	/**
+	 * This method has the responsibility for initializing the 
+	 * interactors in the application, and taking care of any other 
+	 * initialization that needs to be performed.
+	 */
+	public void init() {
+		// You fill this in
+    }
+    
+  
+    /**
+     * This class is responsible for detecting when the buttons are
+     * clicked or interactors are used, so you will have to add code
+     * to respond to these actions.
+     */
+    public void actionPerformed(ActionEvent e) {
+		// You fill this in as well as add any additional methods
+	}
+
+}
---
+++ cs106a-stanford/Assignment7/FacePamphletCanvas.java
@@ -0,0 +1,52 @@
+/*
+ * File: FacePamphletCanvas.java
+ * -----------------------------
+ * This class represents the canvas on which the profiles in the social
+ * network are displayed.  NOTE: This class does NOT need to update the
+ * display when the window is resized.
+ */
+
+
+import acm.graphics.*;
+import java.awt.*;
+import java.util.*;
+
+public class FacePamphletCanvas extends GCanvas 
+					implements FacePamphletConstants {
+	
+	/** 
+	 * Constructor
+	 * This method takes care of any initialization needed for 
+	 * the display
+	 */
+	public FacePamphletCanvas() {
+		// You fill this in
+	}
+
+	
+	/** 
+	 * This method displays a message string near the bottom of the 
+	 * canvas.  Every time this method is called, the previously 
+	 * displayed message (if any) is replaced by the new message text 
+	 * passed in.
+	 */
+	public void showMessage(String msg) {
+		// You fill this in
+	}
+	
+	
+	/** 
+	 * This method displays the given profile on the canvas.  The 
+	 * canvas is first cleared of all existing items (including 
+	 * messages displayed near the bottom of the screen) and then the 
+	 * given profile is displayed.  The profile display includes the 
+	 * name of the user from the profile, the corresponding image 
+	 * (or an indication that an image does not exist), the status of
+	 * the user, and a list of the user's friends in the social network.
+	 */
+	public void displayProfile(FacePamphletProfile profile) {
+		// You fill this in
+	}
+
+	
+}
