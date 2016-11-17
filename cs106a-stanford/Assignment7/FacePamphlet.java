5cde38f3986a21d53e41a32d8426fdb3e8dd92a7
bmaupin <bmaupin@users.noreply.github.com>
2016-11-17 14:01:04 -0500

    Start setting up UI and add top panel

--- cs106a-stanford/Assignment7/FacePamphlet.java
+++ cs106a-stanford/Assignment7/FacePamphlet.java
@@ -1,36 +1,46 @@
-/* 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/*
  * File: FacePamphlet.java
  * -----------------------
  * When it is finished, this program will implement a basic social network
  * management system.
  */
 
-import acm.program.*;
-import acm.graphics.*;
-import acm.util.*;
-import java.awt.event.*;
-import javax.swing.*;
+public class FacePamphlet extends Application
+        implements FacePamphletConstants {
 
-public class FacePamphlet extends Program 
-					implements FacePamphletConstants {
-
-	/**
-	 * This method has the responsibility for initializing the 
-	 * interactors in the application, and taking care of any other 
-	 * initialization that needs to be performed.
-	 */
-	public void init() {
-		// You fill this in
+    public static void main(String[] args) {
+        launch(args);
     }
-    
-  
-    /**
-     * This class is responsible for detecting when the buttons are
-     * clicked or interactors are used, so you will have to add code
-     * to respond to these actions.
-     */
-    public void actionPerformed(ActionEvent e) {
-		// You fill this in as well as add any additional methods
-	}
+
+    @Override
+    public void start(Stage primaryStage) {
+        setUpUi(primaryStage);
+    }
+
+    private void setUpUi(Stage primaryStage) {
+        FacePamphletCanvas canvas = new FacePamphletCanvas(primaryStage);
+    }
+
+    // /**
+    // * This method has the responsibility for initializing the interactors in
+    // * the application, and taking care of any other initialization that needs
+    // * to be performed.
+    // */
+    // @Override
+    // public void init() {
+    // // You fill this in
+    // }
+    //
+    // /**
+    // * This class is responsible for detecting when the buttons are
+    // * clicked or interactors are used, so you will have to add code
+    // * to respond to these actions.
+    // */
+    // public void actionPerformed(ActionEvent e) {
+    // // You fill this in as well as add any additional methods
+    // }
 
 }
--- cs106a-stanford/Assignment7/FacePamphletCanvas.java
+++ cs106a-stanford/Assignment7/FacePamphletCanvas.java
@@ -1,3 +1,4 @@
+
 /*
  * File: FacePamphletCanvas.java
  * -----------------------------
@@ -6,47 +7,121 @@
  * display when the window is resized.
  */
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
-import acm.graphics.*;
-import java.awt.*;
-import java.util.*;
+// This isn't really a canvas but we'll keep the class name to stay as close to the assignment as possible
+public class FacePamphletCanvas implements FacePamphletConstants {
+    /**
+     * Constructor This method takes care of any initialization needed for the
+     * display
+     */
+    public FacePamphletCanvas(Stage primaryStage) {
+        BorderPane mainPane = new BorderPane();
+        mainPane.setPrefSize(APPLICATION_WIDTH, APPLICATION_HEIGHT);
 
-public class FacePamphletCanvas extends GCanvas 
-					implements FacePamphletConstants {
-	
-	/** 
-	 * Constructor
-	 * This method takes care of any initialization needed for 
-	 * the display
-	 */
-	public FacePamphletCanvas() {
-		// You fill this in
-	}
+        HBox topPane = createTopPane();
+        mainPane.setTop(topPane);
 
-	
-	/** 
-	 * This method displays a message string near the bottom of the 
-	 * canvas.  Every time this method is called, the previously 
-	 * displayed message (if any) is replaced by the new message text 
-	 * passed in.
-	 */
-	public void showMessage(String msg) {
-		// You fill this in
-	}
-	
-	
-	/** 
-	 * This method displays the given profile on the canvas.  The 
-	 * canvas is first cleared of all existing items (including 
-	 * messages displayed near the bottom of the screen) and then the 
-	 * given profile is displayed.  The profile display includes the 
-	 * name of the user from the profile, the corresponding image 
-	 * (or an indication that an image does not exist), the status of
-	 * the user, and a list of the user's friends in the social network.
-	 */
-	public void displayProfile(FacePamphletProfile profile) {
-		// You fill this in
-	}
+        Pane centerPane = createCenterPane();
+        mainPane.setCenter(centerPane);
 
-	
+        Scene scene = new Scene(mainPane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private HBox createTopPane() {
+        HBox topPane = new HBox();
+        topPane.setPadding(new Insets(15, 12, 15, 12));
+        topPane.setSpacing(10);
+        centerContents(topPane);
+
+        addComponentsToBottomPane(topPane);
+
+        return topPane;
+    }
+
+    private void centerContents(HBox hbox) {
+        hbox.setAlignment(Pos.CENTER);
+    }
+
+    private void addComponentsToBottomPane(HBox bottomPane) {
+        Text nameLabel = new Text("Name");
+
+        TextField nameInput = new TextField();
+
+        Button buttonAdd = new Button("Add");
+        buttonAdd.setPrefSize(100, 20);
+        buttonAdd.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                // TODO
+            }
+        });
+
+        Button buttonDelete = new Button("Delete");
+        buttonDelete.setPrefSize(100, 20);
+        buttonDelete.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                // TODO
+            }
+        });
+
+        Button buttonLookup = new Button("Lookup");
+        buttonLookup.setPrefSize(100, 20);
+        buttonLookup.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                // TODO
+            }
+        });
+
+        bottomPane.getChildren().addAll(nameLabel, nameInput, buttonAdd, buttonDelete, buttonLookup);
+    }
+
+    private Pane createCenterPane() {
+        Pane centerPane = new Pane();
+        setWhiteBackground(centerPane);
+
+        return centerPane;
+    }
+
+    private void setWhiteBackground(Pane pane) {
+        pane.setStyle("-fx-background-color: white");
+    }
+
+    /**
+     * This method displays a message string near the bottom of the canvas.
+     * Every time this method is called, the previously displayed message (if
+     * any) is replaced by the new message text passed in.
+     */
+    public void showMessage(String msg) {
+        // You fill this in
+    }
+
+    /**
+     * This method displays the given profile on the canvas. The canvas is first
+     * cleared of all existing items (including messages displayed near the
+     * bottom of the screen) and then the given profile is displayed. The
+     * profile display includes the name of the user from the profile, the
+     * corresponding image (or an indication that an image does not exist), the
+     * status of the user, and a list of the user's friends in the social
+     * network.
+     */
+    public void displayProfile(FacePamphletProfile profile) {
+        // You fill this in
+    }
+
 }
