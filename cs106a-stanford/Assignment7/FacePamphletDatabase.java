a1adb110693d38717d60818b0a0ca2d1d772b213
bmaupin <bmaupin@users.noreply.github.com>
2016-11-25 13:59:02 -0500

    Finish assignment 7 milestone 3

--- cs106a-stanford/Assignment7/FacePamphletDatabase.java
+++ cs106a-stanford/Assignment7/FacePamphletDatabase.java
@@ -1,3 +1,4 @@
+
 /*
  * File: FacePamphletDatabase.java
  * -------------------------------
@@ -6,63 +7,69 @@
  * sensitive, so that "ALICE" and "alice" are NOT the same name.
  */
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class FacePamphletDatabase implements FacePamphletConstants {
+    private Map<String, FacePamphletProfile> profiles = new HashMap<String, FacePamphletProfile>();
 
-	/** 
-	 * Constructor
-	 * This method takes care of any initialization needed for 
-	 * the database.
-	 */
-	public FacePamphletDatabase() {
-		// You fill this in
-	}
-	
-	
-	/** 
-	 * This method adds the given profile to the database.  If the 
-	 * name associated with the profile is the same as an existing 
-	 * name in the database, the existing profile is replaced by 
-	 * the new profile passed in.
-	 */
-	public void addProfile(FacePamphletProfile profile) {
-		// You fill this in
-	}
+    /**
+     * This method adds the given profile to the database. If the name
+     * associated with the profile is the same as an existing name in the
+     * database, the existing profile is replaced by the new profile passed in.
+     */
+    public void addProfile(FacePamphletProfile profile) {
+        profiles.put(profile.getName(), profile);
+    }
 
-	
-	/** 
-	 * This method returns the profile associated with the given name 
-	 * in the database.  If there is no profile in the database with 
-	 * the given name, the method returns null.
-	 */
-	public FacePamphletProfile getProfile(String name) {
-		// You fill this in.  Currently always returns null.
-		return null;
-	}
-	
-	
-	/** 
-	 * This method removes the profile associated with the given name
-	 * from the database.  It also updates the list of friends of all
-	 * other profiles in the database to make sure that this name is
-	 * removed from the list of friends of any other profile.
-	 * 
-	 * If there is no profile in the database with the given name, then
-	 * the database is unchanged after calling this method.
-	 */
-	public void deleteProfile(String name) {
-		// You fill this in
-	}
+    /**
+     * This method returns the profile associated with the given name in the
+     * database. If there is no profile in the database with the given name, the
+     * method returns null.
+     */
+    public FacePamphletProfile getProfile(String name) {
+        return profiles.get(name);
+    }
 
-	
-	/** 
-	 * This method returns true if there is a profile in the database 
-	 * that has the given name.  It returns false otherwise.
-	 */
-	public boolean containsProfile(String name) {
-		// You fill this in.  Currently always returns false.
-		return false;
-	}
+    /**
+     * This method removes the profile associated with the given name from the
+     * database. It also updates the list of friends of all other profiles in
+     * the database to make sure that this name is removed from the list of
+     * friends of any other profile.
+     *
+     * If there is no profile in the database with the given name, then the
+     * database is unchanged after calling this method.
+     */
+    public void deleteProfile(String name) {
+        profiles.remove(name);
 
+        for (String profileName : profiles.keySet()) {
+            if (profileContainsFriend(profileName, name)) {
+                profiles.get(profileName).removeFriend(name);
+            }
+        }
+    }
+
+    private boolean profileContainsFriend(String profileName, String friendName) {
+        Iterator<String> friendsIterator = profiles.get(profileName).getFriends();
+
+        while (friendsIterator.hasNext()) {
+            String profileFriendName = friendsIterator.next();
+
+            if (profileFriendName.equals(friendName)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * This method returns true if there is a profile in the database that has
+     * the given name. It returns false otherwise.
+     */
+    public boolean containsProfile(String name) {
+        return profiles.containsKey(name);
+    }
 }
