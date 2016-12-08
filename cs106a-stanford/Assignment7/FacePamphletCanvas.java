fdd4b7fab557bcdeb2c0da513e654a9010acc314
bmaupin <bmaupin@users.noreply.github.com>
2016-12-08 14:05:12 -0500

    Start work on friends list

--- cs106a-stanford/Assignment7/FacePamphletCanvas.java
+++ cs106a-stanford/Assignment7/FacePamphletCanvas.java
@@ -1,7 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -19,9 +24,11 @@ import javafx.scene.text.Text;
 public class FacePamphletCanvas extends BorderPane implements FacePamphletConstants {
     private static final int MESSAGE_FONT_SIZE = 18;
     private static final int PROFILE_NAME_FONT_SIZE = 24;
+    private static final int PROFILE_FRIENDS_LABEL_FONT_SIZE = 16;
     private static final int PROFILE_FRIENDS_LIST_FONT_SIZE = 16;
     private static final int PROFILE_STATUS_FONT_SIZE = 16;
 
+    private Text profileFriendsLabel;
     private Text profileFriendsList;
     private ImageView profileImage;
     private Text profileNameLabel;
@@ -33,30 +40,41 @@ public class FacePamphletCanvas extends BorderPane implements FacePamphletConsta
      * display
      */
     public FacePamphletCanvas() {
-        VBox leftProfilePane = createLeftProfilePane();
+        profileNameLabel = createProfileNameLabel();
+        this.setTop(profileNameLabel);
 
-        this.setCenter(leftProfilePane);
-        BorderPane.setMargin(leftProfilePane, new Insets(0, 0, 0, LEFT_MARGIN));
+        HBox centerProfilePane = createCenterProfilePane();
+        this.setCenter(centerProfilePane);
 
-        profileFriendsList = new Text();
-        profileFriendsList.setFont(new Font(PROFILE_FRIENDS_LIST_FONT_SIZE));
-
-        this.setRight(profileFriendsList);
-
-        messageLabel = new Label();
-        messageLabel.setFont(new Font(MESSAGE_FONT_SIZE));
-
+        messageLabel = createMessageLabel();
         this.setBottom(messageLabel);
-        BorderPane.setMargin(messageLabel, new Insets(0, 0, BOTTOM_MESSAGE_MARGIN, LEFT_MARGIN));
+    }
+
+    private Text createProfileNameLabel() {
+        Text profileNameLabel = new Text();
+        profileNameLabel.setFont(new Font(PROFILE_NAME_FONT_SIZE));
+        profileNameLabel.setFill(Color.BLUE);
+        BorderPane.setMargin(profileNameLabel, new Insets(TOP_MARGIN, 0, 0, LEFT_MARGIN));
+
+        return profileNameLabel;
+    }
+
+    private HBox createCenterProfilePane() {
+        HBox centerProfilePane = new HBox();
+        BorderPane.setMargin(centerProfilePane, new Insets(0, 0, 0, LEFT_MARGIN));
+
+        VBox leftProfilePane = createLeftProfilePane();
+        VBox rightProfilePane = createRightProfilePane();
+
+        centerProfilePane.getChildren().addAll(leftProfilePane, rightProfilePane);
+
+        return centerProfilePane;
     }
 
     private VBox createLeftProfilePane() {
         VBox leftProfilePane = new VBox();
-
-        profileNameLabel = new Text();
-        profileNameLabel.setFont(new Font(PROFILE_NAME_FONT_SIZE));
-        profileNameLabel.setFill(Color.BLUE);
-        VBox.setMargin(profileNameLabel, new Insets(TOP_MARGIN, 0, 0, 0));
+        HBox.setHgrow(leftProfilePane, Priority.ALWAYS);
+        leftProfilePane.setMaxWidth(Double.MAX_VALUE);
 
         profileImage = new ImageView();
         profileImage.setFitHeight(IMAGE_HEIGHT);
@@ -67,9 +85,41 @@ public class FacePamphletCanvas extends BorderPane implements FacePamphletConsta
         profileStatusLabel.setFont(new Font(PROFILE_STATUS_FONT_SIZE));
         VBox.setMargin(profileStatusLabel, new Insets(STATUS_MARGIN, 0, 0, 0));
 
-        leftProfilePane.getChildren().addAll(profileNameLabel, profileImage, profileStatusLabel);
+        leftProfilePane.getChildren().addAll(profileImage, profileStatusLabel);
+
+        // TODO fix width
+        leftProfilePane.setStyle("-fx-background-color: yellow");
 
         return leftProfilePane;
+    }
+
+    private VBox createRightProfilePane() {
+        VBox rightProfilePane = new VBox();
+        HBox.setHgrow(rightProfilePane, Priority.ALWAYS);
+        rightProfilePane.setMaxWidth(Double.MAX_VALUE);
+
+        profileFriendsLabel = new Text();
+        profileFriendsLabel.setFont(new Font(PROFILE_FRIENDS_LABEL_FONT_SIZE));
+        profileFriendsLabel.setStyle("-fx-font-weight: bold");
+
+        profileFriendsList = new Text();
+        profileFriendsList.setFont(new Font(PROFILE_FRIENDS_LIST_FONT_SIZE));
+
+        rightProfilePane.getChildren().addAll(profileFriendsLabel, profileFriendsList);
+
+        // TODO fix width
+        rightProfilePane.setStyle("-fx-background-color: orange");
+
+        return rightProfilePane;
+    }
+
+    private Label createMessageLabel() {
+        Label messageLabel = new Label();
+        messageLabel.setFont(new Font(MESSAGE_FONT_SIZE));
+
+        BorderPane.setMargin(messageLabel, new Insets(0, 0, BOTTOM_MESSAGE_MARGIN, LEFT_MARGIN));
+
+        return messageLabel;
     }
 
     /**
@@ -97,10 +147,19 @@ public class FacePamphletCanvas extends BorderPane implements FacePamphletConsta
         profileStatusLabel.setText(profile.getStatus());
         profileImage.setImage(profile.getImage());
 
-        // TODO: show friends
+        displayFriends(profile);
+    }
+
+    private void displayFriends(FacePamphletProfile profile) {
+        profileFriendsLabel.setText("Friends:");
+
+        List<String> friendsList = new ArrayList<String>();
+        profile.getFriends().forEachRemaining(friendsList::add);
+        profileFriendsList.setText(String.join("\n", friendsList));
     }
 
     public void clearProfile() {
+        profileFriendsLabel.setText("");
         profileFriendsList.setText("");
         profileImage.setImage(null);
         profileNameLabel.setText("");
